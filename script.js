b19ac2be50bd852896af5ccb83e4fcab0b32ea68
Sabrina <sketelone@gmail.com>
2022-10-11 15:29:22 -0700

    Set up book adding function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
 const submit = document.getElementById('submit');
+const cardContainer = document.querySelector('.card-container');
 
 submit.addEventListener('click', function(event) {
     addBookToLibrary(title, author, pages, read);
@@ -22,19 +23,26 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    var newBook = new Book(title, author, pages, read);
+    if (read.Value =="yes") {
+        var newBook = new Book(title.value, author.value, pages.value, "already read");
+    } else {
+        var newBook = new Book(title.value, author.value, pages.value, "not read yet");
+    }
     console.log(newBook);
-    myLibrary.append(newBook);
+    myLibrary.push(newBook);
     console.log(myLibrary);
     displayBook(newBook);
 }
 
 function displayBook(book) {
+    console.log(book)
     var v = document.createElement('div');
     var t = document.createElement('text');
     t.textContent = book.info();
     v.className = "card";
     v.appendChild(t);
+    cardContainer.appendChild(v);
+    console.log("added new book")
 }
 
 // const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet")
